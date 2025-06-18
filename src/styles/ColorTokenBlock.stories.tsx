b09df5cd5fcd454e082c51f070ccd32f5bc60fcc
kelly6226 <kelly6226@naver.com>
2025-06-18 14:04:38 +0900

    ⚡️ workflows 테스트용 브랜치 변경

--- src/styles/ColorTokenBlock.stories.tsx
+++ src/styles/ColorTokenBlock.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type { Meta } from "@storybook/react";
+import type { Meta } from "@storybook/nextjs-vite";
 import colors from "./Colors";
 
 const meta: Meta = {
