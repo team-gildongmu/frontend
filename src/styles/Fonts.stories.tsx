dbe206c6b761ae3349315012f3b1f11d19ae3aaa
Jyservice781 <gimjuyeong512@gmail.com>
2025-06-26 20:06:37 +0900

    💄 : 스토리북 폰트 설정 업데이트 #6

---
+++ src/styles/Fonts.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { Meta } from "@storybook/nextjs-vite";
+import fonts from "@/styles/Fonts";
+
+const meta: Meta = {
+  title: "Foundations/Fonts",
+};
+
+export default meta;
+
+export const AllTypes = () => {
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", gap: "15px" }}>
+      {Object.entries(fonts).map(([name, value]) => (
+        <div key={name} style={{ width: "120px", textAlign: "center" }}></div>
+      ))}
+    </div>
+  );
+};
