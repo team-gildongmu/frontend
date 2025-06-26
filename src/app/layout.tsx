8805f581f8fc59a1c12c4a188c93d914fd4c6793
Jyservice781 <gimjuyeong512@gmail.com>
2025-06-26 19:47:30 +0900

    💄 : 컬러 업데이트, 레이아웃 마진 변경 #6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
               margin: "0 auto",
               padding: "20px",
               minHeight: "100vh",
+              
             }}
           >
             {children}
