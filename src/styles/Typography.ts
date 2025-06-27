7521d9eab66a8b6ba3d8b9c03f2fc87c5ef3f027
kelly6226 <kelly6226@naver.com>
2025-06-27 17:54:02 +0900

    💄 Typography 파일 생성

---
+++ src/styles/Typography.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { Div } from "@/styles/BaseStyledTags";
+
+
+const Default = styled(Div)`
+  white-space: pre-line;
+
+  word-break: normal;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  letter-spacing: -0.1px;
+`;
+
+const Font = styled(Default)``;
+
+export { Font };
