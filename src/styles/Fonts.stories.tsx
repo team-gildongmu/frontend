55a4c1f205188e8c8ff28560dd22999d4b88583e
Jyservice781 <gimjuyeong512@gmail.com>
2025-06-28 23:45:21 +0900

    🐛 : 폰트 버그 수정 #6

--- src/styles/Fonts.stories.tsx
+++ src/styles/Fonts.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Meta } from "@storybook/nextjs-vite";
-import fonts from "@/styles/Fonts";
+import {fonts} from "@/styles/Fonts"
 
 const meta: Meta = {
   title: "Foundations/Fonts",
@@ -8,10 +8,12 @@ const meta: Meta = {
 
 export default meta;
 
+const {noto_100} = fonts
+
 export const AllTypes = () => {
   return (
     <div style={{ display: "flex", flexWrap: "wrap", gap: "15px" }}>
-      {Object.entries(fonts).map(([name]) => (
+      {Object.entries(noto_100).map(([name]) => (
         <div key={name} style={{ width: "120px", textAlign: "center" }}></div>
       ))}
     </div>
--- src/styles/Fonts.ts
+++ src/styles/Fonts.ts
@@ -1,12 +1,17 @@
 import { Noto_Sans_KR } from 'next/font/google'
 import { Nunito } from 'next/font/google'
 
-const fonts = {
- noto_100: Noto_Sans_KR({weight: "100"}),
- noto_300: Noto_Sans_KR({weight: "300"}),
- noto_500: Noto_Sans_KR({weight: "500"}),
+// 폰트는 반드시 const 로 직접 선언부가 있어야 에러 안남.
+const noto_100 = Noto_Sans_KR({weight: "100"});
+const noto_300 = Noto_Sans_KR({weight: "300"});
+const noto_500 = Noto_Sans_KR({weight: "500"});
 
- nunito_500: Nunito({weight: "800"})
+const nunito_500 = Nunito({weight: "800"})
 
-};
-export default fonts;
+export const fonts = {
+    noto_100,
+    noto_300,
+    noto_500,
+    nunito_500
+}
+
