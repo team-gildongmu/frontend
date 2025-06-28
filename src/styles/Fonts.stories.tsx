84de2b005b115e5f1188f573bd3d5c2794a021c0
Jyservice781 <gimjuyeong512@gmail.com>
2025-06-28 23:57:44 +0900

    💄 :storybook 파일 업로드 #6

--- src/styles/Fonts.stories.tsx
+++ src/styles/Fonts.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Meta } from "@storybook/nextjs-vite";
-import {fonts} from "@/styles/Fonts"
+import { fonts } from "@/styles/Fonts";
 
 const meta: Meta = {
   title: "Foundations/Fonts",
@@ -8,14 +8,23 @@ const meta: Meta = {
 
 export default meta;
 
-const {noto_100} = fonts
+const { noto_100, noto_300, noto_500, nunito_500 } = fonts;
 
 export const AllTypes = () => {
   return (
-    <div style={{ display: "flex", flexWrap: "wrap", gap: "15px" }}>
-      {Object.entries(noto_100).map(([name]) => (
-        <div key={name} style={{ width: "120px", textAlign: "center" }}></div>
-      ))}
+    <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
+      <div style={{ width: "200px", textAlign: "center" }}>
+        <p className={noto_100.className}>Noto 100: 가나다라마바사</p>
+      </div>
+      <div style={{ width: "200px", textAlign: "center" }}>
+        <p className={noto_300.className}>Noto 300: 가나다라마바사</p>
+      </div>
+      <div style={{ width: "200px", textAlign: "center" }}>
+        <p className={noto_500.className}>Noto 500: 가나다라마바사</p>
+      </div>
+      <div style={{ width: "200px", textAlign: "center" }}>
+        <p className={nunito_500.className}>Nunito 500: ABCDEFG</p>
+      </div>
     </div>
   );
 };
