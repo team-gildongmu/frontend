11a9495a899063094b5df0502c50ef44157c703a
Jyservice781 <gimjuyeong512@gmail.com>
2025-06-28 23:49:22 +0900

    🐛 : font subset 설정 추가

--- src/styles/Fonts.ts
+++ src/styles/Fonts.ts
@@ -2,11 +2,11 @@ import { Noto_Sans_KR } from 'next/font/google'
 import { Nunito } from 'next/font/google'
 
 // 폰트는 반드시 const 로 직접 선언부가 있어야 에러 안남.
-const noto_100 = Noto_Sans_KR({weight: "100"});
-const noto_300 = Noto_Sans_KR({weight: "300"});
-const noto_500 = Noto_Sans_KR({weight: "500"});
+const noto_100 = Noto_Sans_KR({weight: "100", subsets: ['latin']});
+const noto_300 = Noto_Sans_KR({weight: "300", subsets: ['latin']});
+const noto_500 = Noto_Sans_KR({weight: "500", subsets: ['latin']});
 
-const nunito_500 = Nunito({weight: "800"})
+const nunito_500 = Nunito({weight: "800", subsets: ['latin']})
 
 export const fonts = {
     noto_100,
