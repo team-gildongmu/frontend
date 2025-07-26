e53f03240a38062fed2b507a2ac0890642c2cbc4
Jyservice781 <gimjuyeong512@gmail.com>
2025-07-26 20:24:50 +0900

    💄 :  헤더 설정 추가

--- src/component/common/Header.tsx
+++ src/component/common/Header.tsx
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
-export const Header = () => <HeaderContainer>헤더 영역입니다</HeaderContainer>;
+export const Header = () => (
+  <HeaderContainer>
+    <h1>AAA</h1>
+    <ul>
+      <li className="btn__depth">
+        <a href="">언어 변경이미지</a>
+      </li>
+      <li className="depth">
+        <a href="">중국어</a>
+        <a href="">한국어</a>
+        <a href="">영어</a>
+      </li>
+    </ul>
+  </HeaderContainer>
+)
 
 const HeaderContainer = styled.header`
   width: 100%;
@@ -9,5 +23,8 @@ const HeaderContainer = styled.header`
   background-color: beige;
   position: fixed;
   top: 0;
-  z-index: 100;
+  z-index: 9999;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 20px;
 `;
