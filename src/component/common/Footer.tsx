6725cec26d570d5d12eaa0bc950e8dd2163c0d6c
Jyservice781 <gimjuyeong512@gmail.com>
2025-08-01 17:34:58 +0900

    💄 : 푸터 퍼블리싱 완료 #29

--- src/component/common/Footer.tsx
+++ src/component/common/Footer.tsx
@@ -1,16 +1,38 @@
+"use client";
+
 import React from "react";
-import styled from "styled-components";
+import { FooterContainer, NavItem, Icon, Label } from "./Footer.styles";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = [
+  { label: "메인", path: "/", index: 0 },
+  { label: "검색", path: "/search", index: 1 },
+  { label: "마이로드", path: "/myroad", index: 2 },
+  { label: "마음길", path: "/mind", index: 3 },
+  { label: "내정보", path: "/profile", index: 4 },
+];
 
 export default function Footer() {
-  return <FooterContainer>Footer</FooterContainer>;
-}
+  const pathname = usePathname();
 
-const FooterContainer = styled.footer`
-  width: 100%;
-  max-width: 780px;
-  height: 47px;
-  background-color: skyblue;
-  position: fixed;
-  bottom: 0;
-  z-index: 100;
-`;
+  return (
+    <FooterContainer>
+      <ul>
+        {navItems.map(({ label, path, index }) => {
+          const isActive = pathname === path;
+          return (
+            <li key={label}>
+              <Link href={path}>
+                <NavItem>
+                  <Icon $index={index} $active={isActive} />
+                  <Label $active={isActive}>{label}</Label>
+                </NavItem>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </FooterContainer>
+  );
+}
