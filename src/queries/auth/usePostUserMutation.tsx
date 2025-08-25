aca17bacd945f8f0fdf589d15ff308b23706365a
kelly6226 <kelly6226@naver.com>
2025-08-25 14:53:07 +0900

    feat(low): KakaoLogin 에 mutation 추가

--- src/queries/auth/usePostUserMutation.tsx
+++ src/queries/auth/usePostUserMutation.tsx
@@ -1,12 +1,16 @@
 import { getKakaoLogin } from "@/api/auth";
 import { useMutation } from "@tanstack/react-query";
+import useAuth from "@/hooks/useAuth";
 
 export const usePostUserMutation = () => {
+  const { authorize } = useAuth();
+
   return useMutation({
     mutationKey: ["fetchUser"],
     mutationFn: (code: string) => getKakaoLogin(code),
     onSuccess: (res) => {
       if (res.success && res.data) {
+        authorize(res.data.accessToken);
         console.log("로그인 성공, 토큰 저장 완료");
       }
     },
