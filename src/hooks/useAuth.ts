8611d612b8cc762de40e6fe2926580c405d9b16f
kelly6226 <kelly6226@naver.com>
2025-08-25 14:38:08 +0900

    ✨ useAuth 훅 추가

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,46 +1,29 @@
+import { useCookies } from "react-cookie";
 import { C } from "@/constant";
-import * as CookieUtils from "@/utils/storage";
-
-import { User } from "@/types";
 
 export default function useAuth() {
-  const checkUserToken = (): boolean => {
-    const userToken = CookieUtils.getCookie(C.AUTH_TOKEN_KEY);
-    return !!userToken;
-  };
+  const [cookies] = useCookies([C.AUTH_TOKEN_KEY]);
+  const authToken = cookies[C.AUTH_TOKEN_KEY];
 
   const getUserToken = () => {
-    return CookieUtils.getCookie(C.AUTH_TOKEN_KEY);
+    return authToken;
   };
 
-  const authorize = (user: User, token: string) => {
+  const authorize = (token: string) => {
     const expirationDate = new Date();
     expirationDate.setDate(expirationDate.getDate() + 365);
-    // 로컬 환경 (개발환경용)
-    const isLocalhost = window.location.hostname === "localhost";
-    const domain = isLocalhost
-      ? "localhost"
-      : CookieUtils.getCookie(C.CREATOR_CODE_KEY);
 
-    CookieUtils.setCookie(C.AUTH_TOKEN_KEY, token, {
+    cookies.set(C.AUTH_TOKEN_KEY, token, {
       path: "/",
-      domain,
       expires: expirationDate,
     });
   };
 
   const logout = async (redirect = "/") => {
-    // 로컬 환경 (개발환경용)
-    const isLocalhost = window.location.hostname === "localhost";
-    const domain = isLocalhost
-      ? "localhost"
-      : CookieUtils.getCookie(C.CREATOR_CODE_KEY);
-    CookieUtils.removeCookie(C.AUTH_TOKEN_KEY, domain, "/");
-
-    localStorage.removeItem(C.COMMUNITY_VIEW_MODE_KEY);
+    cookies.remove(C.AUTH_TOKEN_KEY, { path: "/" });
 
     window.location.replace(redirect);
   };
 
-  return { getUserToken, checkUserToken, authorize, logout };
+  return { getUserToken, authorize, logout };
 }
