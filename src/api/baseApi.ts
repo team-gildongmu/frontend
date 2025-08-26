d743f687446c45411ebf413085ec7460d65d0768
kelly6226 <kelly6226@naver.com>
2025-08-26 15:59:07 +0900

    ✨ axios intercepter 추가

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,6 +1,35 @@
-import axios from "axios";
+import { C } from "@/constant";
+import axios, { AxiosInstance } from "axios";
+
+// 쿠키 가져오기 함수 (브라우저 환경에서만 작동)
+const getCookie = (name?: string) => {
+  if (typeof document !== "undefined") {
+    const match = document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`));
+    return match ? match[2] : null;
+  }
+  return null;
+};
+
+const applyInterceptors = (axiosInstance: AxiosInstance) => {
+  axiosInstance.interceptors.request.use(
+    (request) => {
+      const token = getCookie(C.AUTH_TOKEN_KEY);
+
+      if (token) {
+        request.headers["Authorization"] = `Bearer ${token}`;
+      }
+
+      return request;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+};
 
 export const baseApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
 });
+
+applyInterceptors(baseApi);
