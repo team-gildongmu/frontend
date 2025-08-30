9c1f6175585f7527f3723de7eacca6cfaa8f9927
kelly6226 <kelly6226@naver.com>
2025-08-31 00:29:26 +0900

    ✨ Category 부분 퍼블리싱 완료

--- src/component/myroad/list/Category.tsx
+++ src/component/myroad/list/Category.tsx
@@ -6,57 +6,64 @@ import colors from "@/styles/Colors";
 import Icon from "@/component/common/IconifyIcon";
 
 export default function Category() {
-  const categories = [
+  const [selectedCategory, setSelectedCategory] = useState({ id: 1 });
+
+  const getCategories = () => [
     {
       id: 1,
-      name: "# 힐링",
-      icon: "ph:leaf-fill",
+      name: "#힐링",
+      icon: "tdesign:feel-at-ease-filled",
     },
     {
       id: 2,
-      name: "# 도심속",
-      icon: "ph:buildings-fill",
+      name: "#도심속",
+      icon: "mdi:city",
     },
     {
       id: 3,
-      name: "# 혼자만의 시간",
-      icon: "ph:person-fill",
+      name: "#혼자만의 시간",
+      icon: "streamline-ultimate:single-woman-home-bold",
     },
     {
       id: 4,
-      name: "# 드라이브",
-      icon: "ph:car-fill",
+      name: "#드라이브",
+      icon: "material-symbols:unpaved-road",
     },
     {
       id: 5,
-      name: "# 캠핑",
+      name: "#캠핑",
       icon: "ph:tent-fill",
     },
     {
       id: 6,
-      name: "# 가짜데이터1",
-      icon: "ph:star-fill",
+      name: "#가짜데이터1",
+      icon: "ph:tent-fill",
     },
     {
       id: 7,
-      name: "# 가짜데이터2",
-      icon: "ph:heart-fill",
+      name: "#가짜데이터2",
+      icon: "ph:tent-fill",
     },
     {
       id: 8,
-      name: "# 가짜데이터3",
-      icon: "ph:lightning-fill",
+      name: "#가짜데이터3",
+      icon: "ph:tent-fill",
+    },
+    {
+      id: 9,
+      name: "#가짜데이터4",
+      icon: "ph:tent-fill",
     },
   ];
 
-  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+  const categories = getCategories();
 
   return (
     <Row
       height="47px"
       width="100%"
       border={`1px solid ${colors.gray_300}`}
-      px="30px"
+      px="15px"
       gridGap="6px"
       overflow="auto"
     >
@@ -64,6 +71,8 @@ export default function Category() {
         <CenterColumn
           width="fit-content"
           height="auto"
+          minWidth="fit-content"
+          flexShrink={0}
           key={category.id}
           onClick={() => setSelectedCategory(category)}
           style={{ cursor: "pointer" }}
