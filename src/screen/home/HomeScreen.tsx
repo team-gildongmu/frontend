bf2d17f725f6e9c46c4c4efe1d5d8e4bcdc87a83
kelly6226 <kelly6226@naver.com>
2025-09-04 23:18:19 +0900

    ✨ mainBanner 추가

---
+++ src/screen/home/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Column } from "@/styles/BaseComponents";
+import BannerSection from "@/component/home/BannerSection";
+
+export default function HomeScreen() {
+  return (
+    <Column width="100%" height="100%">
+      <BannerSection />
+    </Column>
+  );
+}
