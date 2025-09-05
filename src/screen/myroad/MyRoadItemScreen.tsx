9cf8b57d606a476e9f3c3c7d52291f477ef2b836
kelly6226 <kelly6226@naver.com>
2025-09-05 14:04:42 +0900

    ✨ Calendar 연동

--- src/screen/myroad/MyRoadItemScreen.tsx
+++ src/screen/myroad/MyRoadItemScreen.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { CenterColumn } from "@/styles/BaseComponents";
+import MyRoadCalendar from "@/component/myroad/listItem/MyRoadCalendar";
 
 export default function MyRoadItemScreen({ myroadid }: { myroadid: string }) {
   return (
     <CenterColumn>
       <p>Detail ID: {myroadid}</p>
+      <MyRoadCalendar />
     </CenterColumn>
   );
 }
