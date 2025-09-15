1313bde952f710d44db7237180ed3dfed27b3585
kelly6226 <kelly6226@naver.com>
2025-09-15 17:59:15 +0900

    ✨ listItemContainer theme 추가

---
+++ src/hooks/useMyRoadCategory.ts
@@ -0,0 +1,52 @@
+export const getCategories = () => [
+  {
+    id: 1,
+    name: "자연",
+    icon: "mdi:tree",
+  },
+  { 
+    id: 2,
+    name: "도심",
+    icon: "mdi:city",
+  },
+  {
+    id: 3,
+    name: "야경",
+    icon: "mdi:weather-night",
+  },
+  {
+    id: 4,
+    name: "역사",
+    icon: "mdi:castle",
+  },
+  {
+    id: 5,
+    name: "맛집",
+    icon: "mdi:food",
+  },
+  {
+    id: 6,
+    name: "힐링",
+    icon: "tdesign:feel-at-ease-filled",
+  },
+  {
+    id: 7,
+    name: "신나게",
+    icon: "mdi:party-popper",
+  },
+  {
+    id: 8,
+    name: "쇼핑",
+    icon: "mdi:shopping",
+  },
+  {
+    id: 9,
+    name: "문화/예술",
+    icon: "mdi:palette",
+  },
+  {
+    id: 10,
+    name: "휴양/휴식",
+    icon: "mdi:beach",
+  },
+];
