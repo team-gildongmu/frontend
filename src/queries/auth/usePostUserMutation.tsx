1a467f74c0d2d840504808b1c3b07829f2f84174
kelly6226 <kelly6226@naver.com>
2025-09-17 11:33:20 +0900

    🐛 쿼리 이슈 수정

--- src/queries/auth/usePostUserMutation.tsx
+++ src/queries/auth/usePostUserMutation.tsx
@@ -1,15 +1,17 @@
 import { getKakaoLogin } from "@/api/auth";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import useAuth from "@/hooks/useAuth";
 
 export const usePostUserMutation = () => {
   const { authorize } = useAuth();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationKey: ["fetchUser"],
     mutationFn: (code: string) => getKakaoLogin(code),
     onSuccess: (res) => {
       authorize(res.access_token, res.refresh_token);
+      queryClient.invalidateQueries();
     },
   });
 };
