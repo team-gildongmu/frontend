0203b8bb6615cbc5da95c377c818ec98ed2b5981
kelly6226 <kelly6226@naver.com>
2025-09-17 11:33:20 +0900

    🐛 쿼리 이슈 수정

--- src/queries/travel/useGetLogDetail.tsx
+++ src/queries/travel/useGetLogDetail.tsx
@@ -1,15 +1,12 @@
 import { getLogDetail } from "@/api/travel";
-import useAuth from "@/hooks/useAuth";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 export default function useGetLogDetailQuery(travel_log_id: number) {
-  const { getUserToken } = useAuth();
 
   return useQuery({
     queryKey: ["logDetail", travel_log_id],
     queryFn: () => getLogDetail(travel_log_id),
     placeholderData: keepPreviousData,
     staleTime: 1000 * 60,
-    enabled: !!getUserToken(),
   });
 }
--- src/queries/travel/useLogMapInfo.tsx
+++ src/queries/travel/useLogMapInfo.tsx
@@ -1,15 +1,11 @@
 import { getLogMapInfo } from "@/api/travel";
-import useAuth from "@/hooks/useAuth";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
 export default function useGetLogMapInfoQuery(travel_log_id: number) {
-  const { getUserToken } = useAuth();
-
   return useQuery({
     queryKey: ["logMapInfo", travel_log_id],
     queryFn: () => getLogMapInfo(travel_log_id),
     placeholderData: keepPreviousData,
     staleTime: 1000 * 60,
-    enabled: !!getUserToken(),
   });
 }
