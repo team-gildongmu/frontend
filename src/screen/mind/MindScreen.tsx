d81bfc45f8824b6f1567a35eecc1cb751c6dc38c
Jyservice781 <gimjuyeong512@gmail.com>
2025-09-18 17:35:08 +0900

    💄 : 깃 충돌 해결 #59

--- src/screen/mind/MindScreen.tsx
+++ src/screen/mind/MindScreen.tsx
@@ -23,9 +23,6 @@ export default function MindScreen () {
                 setSelectedCategory={setSelectedCategory}
             />
             <MindCardWrap/>
-            <PlusButtonWrap>
-                <PlusButton />
-            </PlusButtonWrap>
         </MindWrap>
     )   
 }
